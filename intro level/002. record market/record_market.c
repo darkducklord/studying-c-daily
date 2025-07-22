59aee7a53618618e5f37c35bdc7c3328fae53cd5
Misha Nekrasov <mishanekrasov7@gmail.com>
2025-07-22 13:22:35 +0300

    Add support for multiple records in record market
    Refactored print_record to include an index and updated main to display two records with their respective details and prices. Improved formatting for output and variable naming for clarity.

--- intro level/002. record market/record_market.c
+++ intro level/002. record market/record_market.c
@@ -1,25 +1,41 @@
 #include <stdio.h>
 
 float calculate_price(float record_base_price, float markup_percent) {
+
     return record_base_price * (1 + markup_percent / 100);
+
 }
 
-void print_record(const char record_artist[], const char record_title[], int record_year, float final_price) {
-    printf("%s – %s (%d)\n", record_artist, record_title, record_year);
-    printf("Price: %.2f $\n", final_price);
+void print_record(int index,
+    const char record_artist[], const char record_title[], 
+    int record_year, float final_price) {
+
+    printf("%02d. %s – %s (%4d)\n", 
+        index, record_artist, record_title, record_year);
+    printf("Price: %.2f $\n\n", final_price);
+
 }
 
 int main() {
-    const float markup_percent = 30.0f;
-
-    char record_artist[] = "The Beatles";
-    char record_title[] = "Abbey Road Anniversary";
-    int record_year = 2019;
-    float record_base_price = 17.1f;
-
-    float final_price = calculate_price(record_base_price, markup_percent);
+    const float markup_percent = 30.00f;
 
     printf("Records in stock\n\n");
-    print_record(record_artist, record_title, record_year, final_price);
+
+    char record_artist1[] = "The Beatles";
+    char record_title1[] = "Abbey Road Anniversary";
+    int record_year1 = 2019;
+    float record_base_price1 = 17.68f;
+
+    float final_price1 = calculate_price(record_base_price1, markup_percent);
+    print_record(1, record_artist1, record_title1, record_year1, final_price1);
+
+    char record_artist2[] = "Tame Impala";
+    char record_title2[] = "Currents";
+    int record_year2 = 2019;
+    float record_base_price2 = 19.95f;
+
+    float final_price2 = calculate_price(record_base_price2, markup_percent);
+    print_record(2, record_artist2, record_title2, record_year2, final_price2);
     return 0;
+
 }
