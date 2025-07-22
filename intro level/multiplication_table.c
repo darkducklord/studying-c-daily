ea3bf675c33d27ba6bbab043c1f842344f42e721
Misha Nekrasov <mishanekrasov7@gmail.com>
2025-07-22 15:05:46 +0300

    feat: implement multiplication table logic in C program

--- intro level/multiplication_table.c
+++ intro level/multiplication_table.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
 int main() {
+    int i;
+    int n = 1;
+
     printf("Multiplication Table 10x10\n\n");
+
+    while (n <= 10){
+        for (i = 1; i <= 10; i++) {
+            printf("%4d", i * n);
+        }
+        printf("\n");
+        n++;
+    }
+
+    return 0;
 }
